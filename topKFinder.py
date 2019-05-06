c62721f6f90a84a824ba80d1590673badbaf470d
Saeid Ghafouri <sd_ghafouri@yahoo.com>
2019-05-06 16:52:57 +0430

    in degree and out degree files fixed in topKFinder initialVals function

--- topKFinder.py
+++ topKFinder.py
@@ -13,9 +13,14 @@ class topKFinder:
     def findingNeighbors(self):
         for i in range(0,self.numOfNodes):
             self.inNeighbors[i] = np.nonzero(self.graph[:,i])[0]
-        print(self.inNeighbors[5])
+
 
     def initialVals(self):
+        self.indegree = [np.sum(row) for row in self.graph]
+        self.outdegree = [np.sum(row) for row in np.transpose(self.graph)]
+        print(self.indegree)
+        print(self.outdegree)
+
 
 
 
