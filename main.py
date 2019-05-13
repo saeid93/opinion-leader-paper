9b7d6f9309d1ec813be30753f9530695bb0a2162
Saeid Ghafouri <sd_ghafouri@yahoo.com>
2019-05-14 02:25:25 +0430

    centrality methods added

--- main.py
+++ main.py
@@ -2,21 +2,46 @@
 from reader import reader
 from topKFinder import topKFinder
 from otherTopK import otherTopK
+import time
+
+def write2file(file,seed):
+    f= open(file,"w+")
+    for item in seed:
+         f.write(str(item) + "\n")
+    f.close()
+
 
 def main():
+    start = time.time()
+    option = int(input("Enter the right option:\n1. Find top users\n2.Opinion formation test\n"))
+    # algorithm parameters
     alpha = 0.5
-    numOfNodes = 6541
-    # if index is from 1
-    numOfNodes = numOfNodes + 1
     seedSize = 100
-    read = reader('Datasets/advogato.txt', numOfNodes)
+    # dataset data
+    numOfNodes = 6541
+    numOfNodes = numOfNodes + 1 # if index is from 1
+    dataset = "advogato"
+    read = reader("Datasets/" + dataset + ".txt", numOfNodes)
     data = read.reader()
-    # topK = topKFinder(data,seedSize,numOfNodes,alpha)
-    # topKNodes = topK.topKAlg()
-    # print(topKNodes)
 
-    oTopK = otherTopK(data,seedSize)
-    oTopK.findTopKSeeds()
+    # top-k nodes finder
+    if option == 1:
+        # proposed method
+        topK = topKFinder(data,seedSize,numOfNodes,alpha)
+        topKNodes = topK.topKAlg()
+        write2file("topKNodes/" + dataset + "/proposedMethod.csv",topKNodes)
+        # centrality-based methods
+        oTopK = otherTopK(data, seedSize)
+        [BetCenSeed,ClosCenSeed,EigenCenSeed,PageRankCenSeed] = oTopK.findTopKSeeds()
+        write2file("topKNodes/" + dataset + "/betweeness.csv", BetCenSeed)
+        write2file("topKNodes/" + dataset + "/closeness.csv", ClosCenSeed)
+        write2file("topKNodes/" + dataset + "/eigenvector.csv", EigenCenSeed)
+        write2file("topKNodes/" + dataset + "/pagerank.csv", PageRankCenSeed)
+    # print(topKNodes)
+    elif option == 2:
+        print("option2")
+    end = time.time()
+    print((end - start)/60)
 
 if __name__== '__main__':
     main()
--- otherTopK.py
+++ otherTopK.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from operator import itemgetter
 
 
 class otherTopK:
@@ -7,9 +8,32 @@ class otherTopK:
         self.k = k
 
     def findTopKSeeds(self):
-        # print(self.graphAdj)
         graph = nx.DiGraph(self.graphAdj)
-        print(len(graph.edges.data()))
-            # print
+
+        # Betweenes centrality
+        BetCen = nx.betweenness_centrality(graph)
+        BetCenSeed = sorted(BetCen.items(), key=itemgetter(1), reverse=True)[:self.k]
+        BetCenSeed = list(map(lambda x: x[0], BetCenSeed))
+        print("betweenes centrality computed 2/5")
+
+        # Closeness centrality
+        ClosCen = nx.closeness_centrality(graph)
+        ClosCenSeed = sorted(ClosCen.items(), key=itemgetter(1), reverse=True)[:self.k]
+        ClosCenSeed = list(map(lambda x: x[0], ClosCenSeed))
+        print("closeness centrality computed 3/5")
+
+        # Eigenvector centrality
+        EigenCen = nx.eigenvector_centrality(graph)
+        EigenCenSeed = sorted(EigenCen.items(), key=itemgetter(1), reverse=True)[:self.k]
+        EigenCenSeed = list(map(lambda x: x[0], EigenCenSeed))
+        print("eigenvetor centrality computed 4/5")
+
+        # PageRank centrality
+        PageRankCen = nx.pagerank(graph)
+        PageRankCenSeed = sorted(PageRankCen.items(), key=itemgetter(1), reverse=True)[:self.k]
+        PageRankCenSeed = list(map(lambda x: x[0], PageRankCenSeed))
+        print("betweenes centrality computed 5/5")
+
+        return [BetCenSeed,ClosCenSeed,EigenCenSeed,PageRankCenSeed]
 
         # print
